cf7df7519a5934865000b712af3c3980a16995ab
WaldirMarques <waldir.marques@dcx.ufpb.br>
2020-12-05 21:40:09 -0300

    start project

---
+++ src/main/java/br/com/scheduleremail/SchedulerEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.scheduleremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerEmailApplication.class, args);
+	}
+
+}
